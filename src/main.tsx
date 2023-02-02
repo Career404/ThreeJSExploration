9376dd62990db44fc18d13734f7a4c188fd3e9cb
Career404 <105638831+Career404@users.noreply.github.com>
2023-02-02 14:13:53 +0400

    feat: add light, tweak material

--- src/main.tsx
+++ src/main.tsx
@@ -15,12 +15,7 @@ const scene = new THREE.Scene()
 renderer.render(scene, mainCamera)
 const center = new THREE.Vector3()
 scene.updateMatrixWorld(true)
-scene.traverse((object) => {
-	if (object.geometry) {
-		object.geometry.computeBoundingBox()
-		object.geometry.boundingBox.getCenter(center)
-	}
-})
+
 const cameraDistance = 2
 let theta = 0
 mainCamera.position.y = center.y + cameraDistance
@@ -28,24 +23,22 @@ mainCamera.position.z = center.z + cameraDistance
 mainCamera.lookAt(center)
 
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.LineDashedMaterial({
-	color: 0x00ff00,
-	linewidth: 0.1,
-	dashSize: 3,
-	gapSize: 1,
+const material = new THREE.MeshPhongMaterial({
+	color: 0x909900,
 })
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 const geometry2 = new THREE.BoxGeometry(0.5, 0.5, 2)
-const material2 = new THREE.LineDashedMaterial({
-	color: 0x00f1f0,
-	linewidth: 0.1,
-	dashSize: 3,
-	gapSize: 1,
+const material2 = new THREE.MeshPhongMaterial({
+	color: 0x11aaff,
 })
 const cube2 = new THREE.Mesh(geometry2, material2)
 cube2.position.x = 0
 scene.add(cube2)
+
+const light = new THREE.DirectionalLight(0xffff99, 1.5)
+light.position.set(1, 0.1, 0.5)
+scene.add(light)
 
 const cube2target = {
 	positiveLimit: 1,
@@ -62,8 +55,8 @@ function animate() {
 		: false
 
 	cube2target.direction
-		? (cube2.position.x += 0.01)
-		: (cube2.position.x -= 0.01)
+		? (cube2.position.x += 0.002)
+		: (cube2.position.x -= 0.002)
 	mainCamera.position.x = center.x + cameraDistance * Math.sin(theta)
 	mainCamera.position.z = center.z + cameraDistance * Math.cos(theta)
 	mainCamera.lookAt(center)
