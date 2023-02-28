caf52f26f3bec20b815cb1ddb83d7ba855c1f54a
Career404 <105638831+Career404@users.noreply.github.com>
2023-02-28 13:03:13 +0400

    feat: explore drei - on/off camera, grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,56 @@
-/* eslint-disable */
-import * as THREE from 'three'
-import { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { CameraControls } from '@react-three/drei'
-
-function Box(props: JSX.IntrinsicElements['mesh']) {
-	const ref = useRef<THREE.Mesh>(null!)
-	const [hovered, hover] = useState(false)
-	const [clicked, click] = useState(false)
-	// Rotate mesh every frame, this is outside of React without overhead
-	useFrame((state, delta) => (ref.current.rotation.x += delta))
-
-	return (
-		<mesh
-			{...props}
-			ref={ref}
-			scale={clicked ? 1.5 : 1}
-			onClick={(event) => {
-				event.stopPropagation()
-				click(!clicked)
-			}}
-			onPointerOver={(event) => {
-				event.stopPropagation()
-				hover(true)
-			}}
-			onPointerOut={(event) => {
-				event.stopPropagation()
-				hover(false)
-			}}
-		>
-			<boxGeometry args={[1, 1, 1]} />
-			<meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-		</mesh>
-	)
-}
+import { useState, useRef } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { CameraControls, Grid } from '@react-three/drei'
+import Box from './Box'
+import './App.css'
 
 export default function App() {
+	const [camera, setCamera] = useState(true)
+	const [linkHover, setLinkHover] = useState(false)
+
+	const cameraControlRef = useRef<CameraControls | null>(null)
 	return (
-		<Canvas>
-			<CameraControls />
-			<ambientLight intensity={0.01} />
-			<spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-			<pointLight position={[-10, -10, -10]} />
-			<Box position={[-1.2, 0, 0]} />
-			<Box position={[1.2, 0, 0]} />
-		</Canvas>
+		<>
+			<div className="controls">
+				<label htmlFor="cameraCheckbox">
+					Camera controls: {camera ? 'on' : 'off'}
+					<input
+						type="checkbox"
+						name="camera"
+						id="cameraCheckbox"
+						checked={camera}
+						onChange={() => {
+							setCamera(!camera)
+						}}
+					/>
+				</label>
+			</div>
+			<Canvas>
+				<Grid infiniteGrid />
+				{camera ? <CameraControls ref={cameraControlRef} /> : null}
+
+				<ambientLight intensity={0.1} />
+				<spotLight position={[10, 10, 10]} angle={0.08} penumbra={0.5} />
+				<pointLight position={[-10, -10, -10]} />
+
+				<Box position={[-1, 0, 0]} />
+				<mesh
+					onClick={(e) => {
+						e.stopPropagation()
+						cameraControlRef.current?.rotate(Math.PI / 4, 0, true)
+					}}
+					onPointerOver={() => setLinkHover(true)}
+					onPointerOut={() => setLinkHover(false)}
+				>
+					<boxBufferGeometry attach="geometry" args={[1, 0.5, 0.5]} />
+					<meshStandardMaterial
+						attach="material"
+						color={!linkHover ? 'salmon' : 'red'}
+						wireframe={linkHover}
+					/>
+				</mesh>
+				<Box position={[1, 0, 0]} />
+			</Canvas>
+		</>
 	)
 }
